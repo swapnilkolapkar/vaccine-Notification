da39961b216331c4b5416225e7e13daf10307154
Swapnil Kolapkar <37700352+swapnilkolapkar@users.noreply.github.com>
2021-05-09 17:37:08 +0530

    Update vaccineNotifier.js

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -3,16 +3,7 @@ const moment = require('moment');
 const cron = require('node-cron');
 const axios = require('axios');
 const notifier = require('./notifier');
-/**
-Step 1) Enable application access on your gmail with steps given here:
- https://support.google.com/accounts/answer/185833?p=InvalidSecondFactor&visit_id=637554658548216477-2576856839&rd=1
 
-Step 2) Enter the details in the file .env, present in the same folder
-
-Step 3) On your terminal run: npm i && pm2 start vaccineNotifier.js
-
-To close the app, run: pm2 stop vaccineNotifier.js && pm2 delete vaccineNotifier.js
- */
 
 const PINCODE = process.env.PINCODE
 const EMAIL = process.env.FROM_EMAIL
